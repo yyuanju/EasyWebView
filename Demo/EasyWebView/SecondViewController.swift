7ba302b7b70cb46281f3bbfc9d1ad37454739449
HTCHUNGC <HTCHUNGC@tsmc.com>
2022-11-23 08:25:21 +0800

    update secondviewcontroller

--- Demo/EasyWebView/SecondViewController.swift
+++ Demo/EasyWebView/SecondViewController.swift
@@ -16,7 +16,7 @@ class SecondViewController: WebViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.bringSubviewToFront(toolView)
-        urlString = "http://mdetail.tmall.com/templates/pages/desc?id=604372762402"
+        urlString = "https://www.google.com/"
     }
 
     @IBAction func back(_ sender: Any) {
